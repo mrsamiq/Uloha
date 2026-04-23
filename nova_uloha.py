a4dd0c1a4b401482e55349fb2af194299713d4b1
mrsamiq <trpis.samuel@gmail.com>
2026-04-23 08:05:37 +0200

    Pridanie druhej funkcie
    Rozsirenie tejto premakanej aplikacie

--- nova_uloha.py
+++ nova_uloha.py
@@ -4,3 +4,13 @@ for r in range(n):
     for s in range(n):
         print("x", end="")
     print()
+
+n = int(input("Zadaj sirku: "))
+
+for r in range(n):
+    for s in range(n):     
+        if (r + s) % 2 == 0:
+            print("x", end="")
+        else:
+            print(" ", end="")
+    print()
